98399ca90d9d2590e1293e7cd34518b6a9c1b583
Richard Shin <ricshin@cs.berkeley.edu>
2017-08-18 20:10:38 -0700

    Use scripts instead of console_scripts in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='getgpu',
-    version='0.0.1',
+    version='0.0.2',
     description='Return the ID of a free GPU.',
     url='https://github.com/rshin/getgpu',
     install_requires=['nvidia-ml-py~=7.352.0'],
     extras_require={'dev': ['flake8 ~= 3.4.1', 'pre-commit ~= 0.16.3']},
-    entry_points={
-        'console_scripts': [
-            'getgpu = getgpu:main',
-        ],
-    }, )
+    scripts=['getgpu'], )
