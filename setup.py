c699758ad0bb222375d92bdb0420fbcbc1e4b28a
Richard Shin <ricshin@cs.berkeley.edu>
2017-08-22 16:46:38 -0700

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='getgpu',
-    version='0.0.2',
+    version='0.0.3',
     description='Return the ID of a free GPU.',
     url='https://github.com/rshin/getgpu',
     install_requires=['nvidia-ml-py~=7.352.0'],
